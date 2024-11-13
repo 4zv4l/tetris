82c8679990f1d79962797a7f292783d006dff67d
4zv4l <4zv4l@protonmail.com>
2024-11-13 20:22:00 +0800

    init: setup basic board

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,18 @@
 const std = @import("std");
+const ui = @import("ui.zig");
 
-pub fn main() !void {}
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+    var bout = std.io.bufferedWriter(stdout);
+
+    var board: ui.Board = undefined;
+    @memset(&board, [_]u8{ ' ', '.' } ** (ui.cols / 2));
+
+    try ui.init(bout.writer());
+    try ui.drawBoard(bout.writer(), board);
+    try bout.flush();
+    std.time.sleep(std.time.ns_per_s * 5);
+
+    try ui.deinit(bout.writer());
+    try bout.flush();
+}
--- src/ui.zig
+++ src/ui.zig
@@ -1,14 +1,19 @@
 const std = @import("std");
 
+pub const cols = 20;
+pub const lines = 25;
+pub const Board = [lines][cols]u8;
+
 pub const left_border = "<!";
 pub const right_border = "!>";
+pub const square = "[]";
 
 pub const escape = struct {
-    const ENTER_ALTER = "x1b[?1049h";
-    const EXIT_ALTER = "x1b[?1049l";
-    const CLEAR_TOP_LEFT = "x1b[2Jx1b[H";
-    const SHOW_CURSOR = "x1b[?25h";
-    const HIDE_CURSOR = "x1b[?25l";
+    const ENTER_ALTER = "\x1b[?1049h";
+    const EXIT_ALTER = "\x1b[?1049l";
+    const CLEAR_TOP_LEFT = "\x1b[2J\x1b[H";
+    const SHOW_CURSOR = "\x1b[?25h";
+    const HIDE_CURSOR = "\x1b[?25l";
 };
 
 pub fn init(writer: anytype) !void {
@@ -17,4 +22,13 @@ pub fn init(writer: anytype) !void {
 
 pub fn deinit(writer: anytype) !void {
     try writer.print("{s}{s}", .{ escape.EXIT_ALTER, escape.SHOW_CURSOR });
+}
+
+pub fn drawBoard(writer: anytype, board: Board) !void {
+    try writer.print("{s}", .{escape.CLEAR_TOP_LEFT});
+    for (board) |line| {
+        try writer.print("{s}", .{left_border});
+        try writer.print("{s}", .{line});
+        try writer.print("{s}\n", .{right_border});
+    }
 }
